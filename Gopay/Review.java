19c6068c04e6e66abfeed4cdb7f2140385062984
Rafliindragunawan <145978525+Rafliindragunawan@users.noreply.github.com>
2025-03-26 20:51:54 +0700

    Add files via upload

---
+++ Gopay/Review.java
@@ -0,0 +1,13 @@
+public class Review {
+    private String idReview;
+    private String idUser;
+    private String isiReview;
+
+    public void setReview(String idReview, String idUser, String isiReview) {
+        this.idReview = idReview;
+        this.idUser = idUser;
+        this.isiReview = isiReview;
+    }
+}
+
+
